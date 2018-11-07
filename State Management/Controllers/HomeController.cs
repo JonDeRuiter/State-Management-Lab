1787bb4f460ed9038327ebc0110982bb773e6ff8
Jon DeRuiter <jonathanhderuiter@gmail.com>
2018-11-07 14:41:33 -0500

    got it all up and running

---
+++ State Management/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using State_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace State_Management.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(UserClass user)
+        {
+            if (Session["Name"] == null)
+            {
+                Session["Name"] = user.userName;
+                Session["Email"] = user.email;
+                Session["Password"] = user.pw;
+                Session["Age"] = user.age;
+            }
+            
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.goodbye = $"Goodbye, {Session["Name"]}";
+            Session["Name"] = null;
+
+            return View();
+        }
+    }
+}
---
+++ State Management/Models/UserClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace State_Management.Models
+{
+    public class UserClass
+    {
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string pw { get; set; }
+        public int age { get; set; }
+
+    }
+}
